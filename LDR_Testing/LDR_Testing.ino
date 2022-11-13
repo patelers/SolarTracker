45dee946685fb898914c2e2ae1a914992956e090
patelers <79209371+patelers@users.noreply.github.com>
2022-11-13 18:19:35 +0000

    Create LDR_Testing.ino

---
+++ LDR_Testing/LDR_Testing.ino
@@ -0,0 +1,27 @@
+int light, analogLight;
+void setup() {
+  // put your setup code here, to run once:
+  pinMode (13, OUTPUT); // Led is connected to pin 13
+  Serial.begin(9600);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  light = analogRead(A0); //will gain an ADC value from this function: between 0 and 1023
+  Serial.print("Digital value (ADC): ");
+  Serial.print(light);
+  delay(100);
+  analogLight = light * 4.887 ; // ADC * Step size * 10^-3
+  Serial.print(" , Analog Input voltage: ");
+  Serial.print(analogLight);
+  Serial.println(" mV");
+
+  if (light < 40) {  //if value of LDR is less than 40 then LED turns on
+    digitalWrite(13, HIGH);
+  }
+  else {
+    digitalWrite(13, LOW);
+  }
+  Serial.println(light);
+  delay(100);
+}
